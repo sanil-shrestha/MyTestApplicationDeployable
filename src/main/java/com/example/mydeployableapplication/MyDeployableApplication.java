072f726a24e2ce1ece77b3b9542bf926b0d5a860
sanilshrmiu <129837713+sanilshrmiu@users.noreply.github.com>
2023-11-16 00:28:12 -0500

    first commit

---
+++ src/main/java/com/example/mydeployableapplication/MyDeployableApplication.java
@@ -0,0 +1,13 @@
+package com.example.mydeployableapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDeployableApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyDeployableApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mydeployableapplication/MyDeployableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mydeployableapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyDeployableApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
